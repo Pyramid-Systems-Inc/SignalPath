28392d53ee8d21a6ac7d2fbffdfcbf69e9eaeb09
Omar Aglan <omar.aglan91@yahoo.com>
2025-08-03 13:23:53 +0300

    Remove all project files
    Deleted all source, configuration, asset, and build files from the repository. This removes the entire project including frontend, backend, build outputs, and configuration files.

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Grid } from './Grid'
-export { default as Canvas } from './Canvas'
-export { default as ComponentPalette } from './ComponentPalette'
-export { default as PropertiesPanel } from './PropertiesPanel'
--- src/components/symbols/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ResistorSymbol } from './ResistorSymbol';
-export { default as OpAmpSymbol } from './OpAmpSymbol';
-export { default as MicrophoneSymbol } from './MicrophoneSymbol';
